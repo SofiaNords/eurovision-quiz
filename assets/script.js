332e487a3ad0de2f7785f30d50e6c95f4b6d2e90
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-02-12 18:59:36 +0000

    Add the remeinder of questions in 'questions'

--- assets/script.js
+++ assets/script.js
@@ -28,7 +28,179 @@ const questions = [{
             correct: false
         },
     ]
-}];
+}, {
+    question: "Which song did ABBA win the Eurovision Song Contest with in 1974?",
+    answers: [{
+            text: "Mamma Mia",
+            correct: false
+        },
+        {
+            text: "Dancing Queen",
+            correct: false
+        },
+        {
+            text: "Waterloo",
+            correct: true
+        },
+        {
+            text: "S.O.S.",
+            correct: false
+        },
+    ]
+}, {
+    question: "Which Canadian superstar won the Eurovision Song Contest 1988, representing Switzerland?",
+    answers: [{
+            text: "Shania Twain",
+            correct: false
+        },
+        {
+            text: "Avril Lavigne",
+            correct: false
+        },
+        {
+            text: "Céline Dion",
+            correct: true
+        },
+        {
+            text: "Alanis Morissette",
+            correct: false
+        },
+    ]
+}, {
+    question: "Which band won the Eurovision Song Contest 2006 while wearing monster costumes?",
+    answers: [{
+            text: "Rammstein",
+            correct: false
+        },
+        {
+            text: "Lordi",
+            correct: true
+        },
+        {
+            text: "Slipknot",
+            correct: false
+        },
+        {
+            text: "Kiss",
+            correct: false
+        },
+    ]
+}, {
+    question: "Which two countries have won the Eurovision Song Contest the same number of times as each other?",
+    answers: [{
+            text: "Sweden and Ireland",
+            correct: true
+        },
+        {
+            text: "France and United Kingdom",
+            correct: false
+        },
+        {
+            text: "Israel and Netherlands",
+            correct: false
+        },
+        {
+            text: "Luxembourg and Italy",
+            correct: false
+        },
+    ]
+}, {
+    question: "Which year did they start using televoting for the final?",
+    answers: [{
+            text: "1977",
+            correct: false
+        },
+        {
+            text: "1987",
+            correct: false
+        },
+        {
+            text: "1997",
+            correct: true
+        },
+        {
+            text: "2007",
+            correct: false
+        },
+    ]
+}, {
+    question: "Which instrument did Norway’s Alexander Rybak play on stage in his performance of Fairytale?",
+    answers: [{
+            text: "Guitar",
+            correct: false
+        },
+        {
+            text: "Piano",
+            correct: false
+        },
+        {
+            text: "Violin",
+            correct: true
+        },
+        {
+            text: "Accordion",
+            correct: false
+        },
+    ]
+}, {
+    question: "Which non-European country joined the competition in 2015?",
+    answers: [{
+            text: "Australia",
+            correct: true
+        },
+        {
+            text: "Canada",
+            correct: false
+        },
+        {
+            text: "Japan",
+            correct: false
+        },
+        {
+            text: "Morocco",
+            correct: false
+        },
+    ]
+}, {
+    question: "Which country holds the record for the most points received in a single contest?",
+    answers: [{
+            text: "Norway",
+            correct: false
+        },
+        {
+            text: "Portugal",
+            correct: false
+        },
+        {
+            text: "Bulgaria",
+            correct: false
+        },
+        {
+            text: "Ukraine",
+            correct: true
+        },
+    ]
+}, {
+    question: "Which country will host Eurovision 2024?",
+    answers: [{
+            text: "England",
+            correct: false
+        },
+        {
+            text: "Ireland",
+            correct: false
+        },
+        {
+            text: "Sweden",
+            correct: true
+        },
+        {
+            text: "Germany",
+            correct: false
+        },
+    ]
+}
+];
 
 function startQuiz() {
     questionNumber = 0;
@@ -38,6 +210,7 @@ function startQuiz() {
 }
 
 function showQuestion() {
+    resetState();
     let currentQuestion = questions[questionNumber];
     let questionNo = questionNumber + 1;
     question.innerHTML = questionNo + ". " + currentQuestion.question;
