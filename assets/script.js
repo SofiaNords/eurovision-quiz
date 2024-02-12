10796e225f4d7993e2cca7ec32a9ab75e6a34ed4
Sofia Nordström <sofia.k.nordstrom@gmail.com>
2024-02-12 12:49:20 +0000

    Declare 'getters' from index.html, declare question number & score, add first question in script.js

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "Who won the first ever Eurovision Song Contest in 1956?"
+        answers: [
+            {text: "Switzerland", correct: true}
+            {text: "United Kingdom", correct: false}
+            {text: "France", correct: false}
+            {text: "Italy", correct: false}
+        ]
+    }
+]
